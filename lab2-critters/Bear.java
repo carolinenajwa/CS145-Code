83f6e0d175036f33bfac858b7bf91ad46a7bb670
Odyssey-94 <82617020+Odyssey-94@users.noreply.github.com>
2021-04-16 21:11:39 -0700

    Update Bear.java
    Updated Bear Class.

--- lab2-critters/Bear.java
+++ lab2-critters/Bear.java
@@ -1,6 +1,54 @@
 import java.lang.reflect.Constructor;
 
-public class Bear {
+// This defines a simple class of critters that infect whenever they can and
+// otherwise just spin around, looking for critters to infect.  This simple
+// strategy turns out to be surpisingly successful.
 
 
-}
+import java.awt.*;
+
+ 
+   
+// Bear subclass of Critter
+public class Bear extends Critter {
+
+    // Private methods measuring count number of occurrences characters 
+    // and color of Bear
+    private int count;
+    private boolean polar;
+    
+    // Bear class constructor
+    public Bear(boolean polar) {
+         this.count = 0
+         this.polar = polar;
+         
+    }
+    
+    // Returns Bear movement
+    public Action getMove(CritterInfo info) {
+        if (info.getFront() == Neighbor.OTHER) {
+            return Action.INFECT;
+      } if (info.getDirection() == Neighbor.EMPTY){
+            return Action.HOP;
+        }
+     }
+   
+    // Returns Polar Bear/Bear colors
+    public Color getColor() {
+        if (this.polar) {
+            return Color.WHITE;
+       } else { 
+            return Color.BLACK;
+            }
+          }
+
+    // Returns charaters displaying Bear
+    public String toString() {
+        this.count = this.count + 1
+        if (count % 2 == 1) {
+            return "/";
+        } else {
+            return "\\"
+     } 
+     }      
+    }
