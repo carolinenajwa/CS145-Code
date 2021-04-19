f262fb24509ddb8a2c0b2cebcdd5ec5dd693d455
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-19 09:29:17 -0700

    Update Orca.java

--- lab2-critters/Orca.java
+++ lab2-critters/Orca.java
@@ -24,32 +24,13 @@ public class Orca extends Critter {
 
     // return gray color
     public Color getColor() {
-        return Color.GRAY;
+        return Color.RED;
     }
 
     // define the UI element of the critter
     public String toString() {
-        String status;
 
-
-        // move between fee, fie, foe, fum every 6 moves
-        if (this.count <= 6) {
-            status = "fee";
-        } else if (this.count > 6 && this.count <= 12) {
-            status = "fie";
-        } else if (this.count > 12 && this.count <= 18) {
-            status = "foe";
-        } else {
-            status = "fum";
-        }
-
-        this.count++;
-
-        if (this.count >= 24) {
-            this.count = 0;
-        }
-
-        return status;
+        return "WCC";
 
     }
 
