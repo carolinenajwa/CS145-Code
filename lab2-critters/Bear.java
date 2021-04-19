a59e05631109869bd13cf0f71d7e7d5c20f4c362
Odyssey-94 <82617020+Odyssey-94@users.noreply.github.com>
2021-04-19 14:29:28 -0700

    Update Bear.java
    Made style changes and re-organized the order of the methods.

--- lab2-critters/Bear.java
+++ lab2-critters/Bear.java
@@ -1,53 +1,62 @@
-// This defines the BEAR Class. BEARS infect if an enemy is in front. 
-// Otherwise hops or turns left. BEARS flips between BLACK and WHITE and 
+// This defines the Bear Class. Bears infect if an enemy is in front. 
+// Otherwise hops or turns left. Bears flips between BLACK = Bear and WHITE = Polar Bear and 
 // returns a character that alternate on each different move between a 
-// slash character (/) and a backslash character.
+// slash character (/) and a backslash character(\).
 
+import java.lang.reflect.Constructor;
 import java.awt.*;
 
+ 
+   
 // Bear subclass of Critter
 public class Bear extends Critter {
 
-    // Private methods measuring count number of occurrences characters
-    // and color of Bear
-    private int count;
-    private boolean polar;
-
-    // Bear class constructor
-    public Bear(boolean polar) {
-        this.count = 0;
-        this.polar = polar;
-
+   // Private methods measuring count number of occurrences characters 
+   // and color of Bear
+   private int count;
+   private boolean polar;
+    
+   // Bear class constructor
+   public Bear(boolean polar) {
+      this.count = 0;
+      this.polar = polar;      
     }
-
+    
     // Returns Bear movement
     public Action getMove(CritterInfo info) {
+        Action move;
         if (info.getFront() == Neighbor.OTHER) {
-            return Action.INFECT;
-        } else if (info.getFront() == Neighbor.EMPTY) {
-            return Action.HOP;
-        } else {
-            return Action.LEFT;
-
-        }
-    }
-
+            move = Action.INFECT;
+      } else if (info.getFront() == Neighbor.EMPTY) {
+            move = Action.HOP;
+      } else {
+            move = Action.LEFT;
+      }
+        return move;
+     }
+  
+ 
+   
     // Returns Polar Bear/Bear colors
     public Color getColor() {
-        if (this.polar) {
-            return Color.WHITE;
-        } else {
-            return Color.BLACK;
-        }
+       Color setcolor;
+       if (this.polar) {
+            setcolor = Color.WHITE;
+     } else { 
+            setcolor = Color.BLACK;
+     }
+       return setcolor;
     }
 
     // Returns charaters displaying Bear
     public String toString() {
-        this.count = this.count + 1;
-        if (count % 2 == 1) {
-            return "/";
-        } else {
-            return "\\";
-        }
-    }
+       String character;
+       this.count = this.count + 1;
+       if (count % 2 == 1) {
+            character = "/";
+     } else {
+            character = "\\";
+     } 
+       return character;
+    }      
 }
