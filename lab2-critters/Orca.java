e26dc607d0d42aa92c377649436ae4abf1219aa6
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-20 13:53:40 -0700

    add colors

--- lab2-critters/Orca.java
+++ lab2-critters/Orca.java
@@ -1,20 +1,25 @@
 
 // grab stuff we need
 import java.awt.*;
+import java.util.*;
 
 public class Orca extends Critter {
     // count helps us keep track of our iterations
     private int count;
     private String lastMove;
+    private Random rand;
+    private int lastColorCode;
 
     // Orca class with counter
     public Orca() {
         this.count = 0;
+        this.rand = new Random();
     }
 
     // action movement for Giant
     public Action getMove(CritterInfo info) {
         Action move;
+        
 
         if (info.getFront() == Neighbor.OTHER) {
             move = Action.INFECT;
@@ -23,12 +28,8 @@ public class Orca extends Critter {
             move = Action.HOP;
             this.lastMove = "H";
         }else {
-            if(this.count % 2 == 0){
-                move = Action.RIGHT;
-            }else{
-                move = Action.LEFT;
-            }
             
+            move = Action.RIGHT;
             this.lastMove = "L";
         }
 
@@ -38,13 +39,21 @@ public class Orca extends Critter {
     // return gray color
     public Color getColor() {
         Color setColor;
+        int randColorInt;
 
-        if(this.count % 2 == 0){
-            setColor = Color.RED;
+        //we can check if this.lastColorCode is initalized becase we are
+        //adding 1 in the next step
+        if (this.lastColorCode == 0) {
+            randColorInt = this.rand.nextInt(0xffffff + 1);
         }else{
-            setColor =  Color.BLACK;
+            randColorInt = this.lastColorCode + 1;
         }
-
+        
+        //format into hex code. 
+        String colorCode = String.format("#%06x", randColorInt);
+        setColor = Color.decode(colorCode);
+       
+        //setColor = Color.RED;
         this.count++;
 
         return setColor;
