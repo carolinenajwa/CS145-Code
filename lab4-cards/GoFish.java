0aeb4f17f6669c3a7de1c0c1ae6c2c66e2b58ecd
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-23 10:35:57 -0700

    Update GoFish.java

--- lab4-cards/GoFish.java
+++ lab4-cards/GoFish.java
@@ -1,7 +1,27 @@
+import java.util.Stack;
+
 //plays a game of go fish
 
 public class GoFish {
     public static void main(String[] args) {
+        Stack<E> cardStack = new Stack<String>();
+
         
     }
+
+    public static void addCard(Stack cardStack, String card) {
+        cardStack.push(card);
+    }
+
+    public static String removeCard(Stack cardStack) {
+        return cardStack.pop();
+    }
+
+    public static String peekCard(Stack cardStack) {
+        return cardStack.peek();
+    }
+
+
+
+
 }
