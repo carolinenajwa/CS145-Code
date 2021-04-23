91b94cc3f3a5dd4506638b00af8c79d922153abb
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-04-23 13:12:27 -0700

    plan

---
+++ lab4-cards/Card.java
@@ -0,0 +1,18 @@
+public class Deck {
+
+    private int deckSize;
+    public static void main(String[] args) {
+        
+    }
+
+    public static Card() {
+        
+    }
+
+    public static int enum deckSize() {
+        return deckSize;
+    }
+    
+
+
+}
--- lab4-cards/GoFish.java
+++ lab4-cards/GoFish.java
@@ -1,22 +1,59 @@
+import java.util.Random;
 import java.util.Stack;
 
 //plays a game of go fish
+//52 cards 
+//point is to get 4 of the same card
+//each player has 7 cards
+
+
+//ace 
+//2-10
+//jack
+//queen
+//king
+
+
+//shuffle cards into a 52 card stack/queue
+
+
+//deal cards 7 to each player array
+
+
+//Stack 
+//|A|
+//|B|
+//|C|
+
+
 
 public class GoFish {
     public static void main(String[] args) {
-        Stack<E> cardStack = new Stack<String>();
 
+
+        Stack<String> cardStack = new Stack<String>();
+        Rand rand = new Random();
         
+
+
     }
 
+    public static 
+
+
+
+   
+    //add a card to the stack
     public static void addCard(Stack cardStack, String card) {
         cardStack.push(card);
     }
 
+    //get the top card from the stack
     public static String removeCard(Stack cardStack) {
         return cardStack.pop();
     }
 
+    //check the top card in the stack
     public static String peekCard(Stack cardStack) {
         return cardStack.peek();
     }
