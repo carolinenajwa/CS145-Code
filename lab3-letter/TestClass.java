4f2aa5d9dbda03c73e85dcc1405a2f14bebd6305
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-05-07 11:45:37 -0700

    Create TestClass.java

---
+++ lab3-letter/TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass {
+
+    //main to test the main class
+    public static void main(String[] args) {
+        LetterInventory test = new LetterInventoryMain("test");
+        LatterInventory test2 = new LetterInventoryMain("test");
+        test = (text.subtract(test));
+    }
+}
