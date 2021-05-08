5182ee3ca728e3f3dc3fb9fbf0ff7d58926eaa71
Odyssey-94 <82617020+Odyssey-94@users.noreply.github.com>
2021-05-08 13:15:39 -0700

    Update TestClass.java

--- assignment 2/TestClass.java
+++ assignment 2/TestClass.java
@@ -1,21 +1,73 @@
-//TestClass provides the user interface for the phonebookManager. 
+// Test Class
+// User Interface
 
 
-//Import utilities 
+
+
 import java.util.*;
 
+
 public class TestClass {
-    public static void main(String[] args) {
-        //define new phonebook for Bellingham 
-
-        //define new phonebook for Seattle 
-
-        //test all methods of phonebookManager manually. 
-
-        //then, define a TUI (Text User Interface) 
-
-        //move from phonebook a to phonebook b
 
 
-    }
+      Scanner console = new Scanner(System.in);
+
+
+
+ public static void main(String[] args) {
+      LinkedList<TestClass> list = new LinkedList<>();
+      int menuInput;
+      Scanner console = new Scanner(System.in);
+      boolean quit = false;
+         
+         do {
+      
+      
+      System.out.println("Welcome to PhoneBook Manager!");
+      System.out.println("Menu Options: ");
+      System.out.println("1  (Add new contact)");
+      System.out.println("2  (Display contact directory)");
+      System.out.println("3  (Delete contact)");
+      System.out.println("4  (Edit contact)");
+      System.out.println("5  (Quit)");
+
+
+      System.out.println("How can I help you today?");
+      System.out.print("Enter Valid Command: ");
+      menuInput = console.nextInt();
+      
+
+      switch (menuInput) {
+      // Add contact
+      case 1:
+      PhoneBook.addEntry();      
+      break;
+     
+      // Display contacts
+      case 2:
+      PhoneBook.displayDirectory();
+      break;
+                     
+      // Delete contact
+      case 3:
+      PhoneBook.deleteEntry
+      break;
+      
+      // TODO: Modify contact
+      case 4:
+      
+      break;
+      
+      // Quit program   
+      case 5:
+      System.out.println("Goodbye!");
+      quit = true;
+      break;
+      
+      // Default - invalid input
+      default:
+      System.out.println("Invalid Input. Try Again!");
+      }
+ }while (!quit);
+}
 }
